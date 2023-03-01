7afbe58577a8a47bb0a70f91bdd7db2b00f189fc
Fang Limin <fanglimin@Fangs-MacBook-Air.local>
2023-03-01 16:46:39 +0800

    init working version

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+import web3 from "./web3";
+import rental from "./rental";
+
+class App extends React.Component {
+  state = {
+    owner1: "",
+    owner2: "",
+    value: "",
+    message: "",
+  };
+  async componentDidMount() {
+    const owners = await  rental.methods.getOwners();
+    const owner1 = owners[0];
+    const owner2 = owners[1];
+
+    this.setState({ owner1, owner2 });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    // const accounts = await web3.eth.getAccounts();
+
+    const currentAccount = await web3.givenProvider.selectedAddress;
+
+    this.setState({ message: "Waiting on transaction success..." });
+
+    await rental.methods.payRental().send({
+      // from: accounts[0],
+      from: currentAccount,
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+
+    this.setState({ message: "You have payment your rental!" });
+  };
+
+
+  render() {
+    return (
+      <div>
+        <h2>My Rental Portal</h2>
+        <p>
+          The amount paid will be split into the following owners' account directly,
+          Owner1 {this.state.owner1}
+          Owner2 {this.state.owner2}
+        </p>
+
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Ready to Pay?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr />
+
+
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/rental.js
@@ -0,0 +1,48 @@
+import web3 from './web3';
+
+const address = '0xa8caE7fCD1091764558B1D22B0bB9b44919bD848';
+const abi = [
+    {
+        "inputs": [],
+        "name": "payRental",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address payable",
+                "name": "owner1",
+                "type": "address"
+            },
+            {
+                "internalType": "address payable",
+                "name": "owner2",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "getOwners",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+export default new web3.eth.Contract(abi, address);
